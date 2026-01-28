6722e9f7456d5a9ab24940ac19e94e55a646cb53
MeganathanU <udhayameganathan@gmail.com>
2026-01-28 16:08:22 +0530

    project

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "newk8svjbuck" # Replace with your actual S3 bucket name
+    bucket = "chatgpt-project1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "ap-northeast-2"
   }
 }
